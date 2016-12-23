e5643e9f8e72c03590bf96590477d6e3f4de3c10
thanhhnk <thanhhnk@gmail.com>
2016-12-24 00:22:10 +0100

    Compile work

--- LaundryMachine/LaundryMachine.ino
+++ LaundryMachine/LaundryMachine.ino
@@ -51,30 +51,6 @@ void loop()
   mProgramSelect->Poll();
   Ready = mProgramExecutor->IsReady(mProgramSelect->GetProgramType());
   Serial.print("machine is ready for wash: ");Serial.println(Ready); 
-  
-  /*if (mControl->GetCoin10Button())
-  {
-    count++;
-  }
-  if (count == 1)
-  {
-    //Serial.println("First press");
-    mControl->SetCoin10(0x01);
-  }
-  else if (count == 2)
-  {
-    //Serial.println("Second press");
-    mControl->SetCoin10(0x02);
-  }
-  else if (count == 3)
-  {
-    //Serial.println("Third press");
-    mControl->SetCoin10(0x04);
-  }
-  else
-  {
-    count = 0;
-  }*/
 }
 
 
