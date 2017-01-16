2448e0c1946a1f61f2bf6243e6ce2e2af395b363
thanhhnk <thanhhnk@gmail.com>
2017-01-16 22:33:19 +0100

    Updated TestCoinWallet

--- LaundryMachine/Test/TestCoinWallet.cpp
+++ LaundryMachine/Test/TestCoinWallet.cpp
@@ -18,3 +18,19 @@ TEST_F(TestCoinWallet, test_coin10_zero)
     int balance = mCoinWallet->GetAmount();
     EXPECT_EQ(balance, 0);
 }
+
+TEST_F(TestCoinWallet, test_coin10_one)
+{
+    mCoinWallet->AddCoin10();
+    int balance = mCoinWallet->GetAmount();
+    EXPECT_EQ(balance, 10);
+}
+
+TEST_F(TestCoinWallet, test_coin10_two)
+{
+    mCoinWallet->AddCoin10();
+    mCoinWallet->AddCoin10();
+    int balance = mCoinWallet->GetAmount();
+    EXPECT_EQ(balance, 20);
+}
+
