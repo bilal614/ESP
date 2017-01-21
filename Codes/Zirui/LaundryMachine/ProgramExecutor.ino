f1680b0c713e3ae57638b6e6a519ba36fa77b3af
CRH380B-6216L <muner_szr6216@outlook.com>
2017-01-21 20:38:10 +0100

    my conclusion for project

--- Codes/Zirui/LaundryMachine/ProgramExecutor.ino
+++ Codes/Zirui/LaundryMachine/ProgramExecutor.ino
@@ -1,29 +1,172 @@
 #include "ProgramExecutor.h"
+#define longDelay 6000
+#define shortDelay 3000
 
-ProgramExecutor::ProgramExecutor(IBuzzer * b, IMotor * m, ILock * l, ISoap * s, ITemperature * t, IWater * w, CoinWallet * c)
+ProgramExecutor::ProgramExecutor(IBuzzer * b, IMotor * m, ILock * l, ISoap * s, ITemperature * t, IWater * w)
 {
-  mBuzzer = b;
-  mMotor = m;
-  mLock = l;
-  mSoap = s;
-  mTemperature = t;
-  mWater = w;
-  mCoinWallet = c;
+  //mBuzzer.setInterface(b);
+  mMotor.setInterface(m);
+  mLock.setInterface(l);
+  mSoap.setInterface(s);
+  mTemperature.setInterface(t);
+  mWater.setInterface(w);
 }
 
 boolean ProgramExecutor::Start(ProgramSettings * p)
 {
   mProgramSettings = p;
+  char ProgramType = mProgramSettings->GetProgramType();
+  int ProgramCost = mProgramSettings->GetProgramCost();
+  int moneyInWallet = mCoinWallet->GetAmount();
+  mCoinWallet->ReturnChange();
+  if(moneyInWallet >= ProgramCost && StepSwitches())
+  {
+    if(ProgramType == 'A')
+    {
+      //execute program A recipe
+      //Prewash:
+      mWater.SetLevel(2);
+      mSoap.lockCpt1(false);
+      mMotor.rotateLM( 1, 2);
+      tempDelay(longDelay);
+      stopDelay(2);
+      mMotor.rotateLM( 0, 2);
+      tempDelay(longDelay);
+      stopDelay(2);
+      mWater.SetLevel(0);
+      
+      //Main-wash (note: add temperature later)
+      
+      if(mWater.CheckLevel()>0)
+      {
+        mTemperature.SetTemperature(2);
+      }
+       //step 1)
+      mTemperature.Poll();
+      mWater.SetLevel(2);
+      mSoap.lockCpt2(false);
+      mMotor.rotateLM(1, 2);
+      tempDelay(longDelay);
+      //mMotor.Stop();
+      stopDelay(2);
+      
+      mMotor.rotateLM(0, 2);
+      //delay(6000);
+      tempDelay(longDelay);
+      //mMotor.Stop();
+      stopDelay(2);
+      
+      mMotor.rotateLM(1, 2);
+      //delay(6000);
+      tempDelay(6000);
+      //mMotor.Stop();
+      stopDelay(2);
+      
+      mMotor.rotateLM(0, 2);
+      //delay(6000);
+      tempDelay(6000);
+      //mMotor.Stop();
+      stopDelay(2);
+      
+      mWater.SetLevel(0);
+      //step 2)
+      /*
+      mWater.SetLevel(2);
+      mMotor.rotateLM(6000, 1, 2);
+      mMotor.rotateLM(6000, 0, 2);
+      mMotor.rotateLM(6000, 1, 2);
+      mMotor.rotateLM(6000, 0, 2);
+      mWater.SetLevel(0);
+      //step 3)
+      mMotor.Centrifugation();
+      mWater.SetSink(true);
+    */
+    }
+    if(ProgramType == 'B')
+    {
+      //execute program B recipe
+      
+    }
+    if(ProgramType == 'C')
+    {
+      //execute program A recipe
+      
+    }
+    Serial.print("Selected program: ");Serial.println(ProgramType);
+    Serial.println("There is enough money for this wash.");  
+  }
   return (true);
 }
 
-boolean ProgramExecutor::Step()
+boolean ProgramExecutor::StepSwitches()//this function checks if the switches for the soap and door are locked or not and returns true if yes, else false
 {
-  return (true);
+  boolean doorLock = mLock.lockMachine();
+  //delay(1000);
+  boolean soap1Lock = mSoap.checkCpt1();
+  //delay(1000);
+  boolean soap2Lock = mSoap.checkCpt2();
+  if(doorLock && soap1Lock && soap2Lock)
+  {
+    return (true);
+  }
+  else
+  {
+    return false;
+  }
 }
 
-boolean ProgramExecutor::IsReady()
+boolean ProgramExecutor::IsReady(char prog)
 {
-  return (false);
+  mProgramSettings->setProgramAndCost(prog);
+  
+  int walletMoney = mCoinWallet->GetAmount();
+
+  int progCost = mProgramSettings->GetProgramCost();
+  
+  if(walletMoney >= progCost)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
 
+void ProgramExecutor::setCoinWallet(CoinWallet* c)
+{
+  mCoinWallet = c;  
+}
+
+boolean ProgramExecutor::StepCoinWallet()
+{
+  mCoinWallet->Poll();
+}
+
+void ProgramExecutor::stopDelay(int Speed)
+{
+  mMotor.Stop();
+  if(Speed == 3)
+  {
+    delay(5000);
+  }
+  if(Speed == 2)
+  {
+    delay(2000);
+  }
+  if(Speed == 1)
+  {
+    delay(1000);
+  }
+}
+
+
+void ProgramExecutor::tempDelay(int d)
+{
+  int DelayFactor = d/100;
+  for(int i  = 0; i < DelayFactor; i++)
+  {
+    mTemperature.Poll();
+    delay(100);
+  }   
+}
